921d0d2450bb9483ebd3a5c7706a582d12833795
vbirnberg <98720631+vbirnberg@users.noreply.github.com>
2022-05-03 01:17:38 -0500

    Add files via upload

--- mappa/mappedLocationObject.js
+++ mappa/mappedLocationObject.js
@@ -25,14 +25,17 @@ let imageArray = [];
 let locationsArray = [];
 
 
+
+
 function preload(){
   // Load the data
   locations = loadTable('updatedData.csv', 'csv', 'header');
+
 }
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
-  imageMode(CENTER)
+
 
   // Create a tile map and overlay the canvas on top.
   myMap = mappa.tileMap(options);
@@ -45,6 +48,7 @@ function setup() {
   for (let i = 0; i < locations.getRowCount(); i++) {
     imageArray[i] = loadImage('images/' + locations.getString(i, 'image') + '.jpeg');
 
+  }
 }
 
 // The draw loop is fully functional but we are not using it for now.
@@ -76,6 +80,10 @@ function drawLocations() {
     const latitude = Number(locations.getString(i, 'latitude'));
     const longitude = Number(locations.getString(i, 'longitude'));
     const place = String(locations.getString(i,'location'));
+    const hours = String(locations.getString(i, 'active hours'))
+    const date = String(locations.getString(i, 'date'))
+    const steps = String(locations.getString(i, 'steps'))
+
 
     //Only draw them if the position is inside the current map bounds. We use a
     //Leaflet method to check if the lat and lng are contain inside the current
@@ -84,7 +92,8 @@ function drawLocations() {
     if (myMap.map.getBounds().contains({lat: latitude, lng: longitude})) {
       // Transform lat/lng to pixel position
       const pos = myMap.latLngToPixel(latitude, longitude);
-      locationsArray.push(new Locations(pos.x, pos.y, place, imageArray[i]))
+      locationsArray.push(new Locations(pos.x, pos.y, place, imageArray[i], date, hours, steps))
+
     }
   }
   //load all the locationsArray objects and call the show function
@@ -96,27 +105,69 @@ function drawLocations() {
 
 class Locations {
 
-  constructor(long, lat, place,image,){
+  constructor(long, lat, place, image, hours, date, steps){
     this.long = long;
     this.lat = lat;
     this.place = place;
     this.image = image;
+    this.hours = hours
+    this.date = date
+    this.steps = steps
+
 
   }
 
   show(){
+
+
+
+      fill(204, 2, 245)
+      textSize(50)
+      textAlign(CENTER)
+      textFont('Georgia')
+      text('[MAP LIFE]', 730, 60)
+      fill(170, 255, 0)
       ellipse(this.long, this.lat, 10, 10);
-      image(this.image)
+
+      fill(235, 115, 2)
+      textSize(30)
+      text('*map content*', 200,30)
+      fill(7, 89, 232)
+      textSize(20)
+      text('-photo location', 200, 60)
+      text('-active walking hours', 200, 90)
+      text('-date', 200, 110)
+      text('-how many steps i walked', 200, 140)
+      text('-photo of the day', 200, 170)
+      textFont('Georgia')
+      textAlign(CENTER)
+
+
+
+
+      //image(this.image,this.long+150, this.lat, 200, 400)
 
   }
 
   hover(){
-    if(dist(mouseX, mouseY, this.long, this.lat) < 5){
-    fill(0);
-    ellipse(this.long, this.lat, 300, 300)
-    text(this.place, this.long, this.lat+20);
-    image(this.image, this.long+150, this.lat, 200, 400)
+
+      if(dist(mouseX, mouseY, this.long, this.lat) < 5){
+        fill(0)
+        ellipse(this.long, this.lat+40, 180, 180)
+
+        fill(255)
+        textSize(15)
+        textFont('Georgia')
+        textAlign(CENTER)
+        text(this.place, this.long, this.lat+20);
+        text(this.date, this.long, this.lat+40)
+        text(this.hours, this.long, this.lat+60)
+        text(this.steps, this.long, this.lat+80)
+        image(this.image, this.long+200, this.lat-100, 200, 350)
+
+
+
+
+    }
   }
-  }
-}
 }
